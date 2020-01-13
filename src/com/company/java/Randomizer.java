f8ee7d9888eb8ad88cab1845f8cd4647791ab1d6
Victor Gustafsson <dv16vgn@cs.umu.se>
2020-01-13 21:25:47 +0100

    Commented Randomizer class.

--- src/com/company/java/Randomizer.java
+++ src/com/company/java/Randomizer.java
@@ -4,16 +4,38 @@ import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Random;
 
+/**
+ * <h1>XMLDataParser</h1>
+ * A parser class for XML.
+ * <p>
+ * animeList - List of Anime objects.
+ * <p>
+ * outputAmount - Amount of shows to randomize.
+ * <p>
+ *
+ * @author  Victor Gustafsson
+ * @version 1.0
+ * @since   2020-01-13
+ */
+
 public class Randomizer {
 
     private ArrayList<Anime> animeList;
     private int outputAmount = 0;
 
+    /**
+     * Standard constructor.
+     * @param animeList List of Anime objects.
+     */
+
     public Randomizer(ArrayList<Anime> animeList) {
         this.animeList = animeList;
-
         parseConfig();
     }
+
+    /**
+     * Parses the config file and assigns a value to outputAmount.
+     */
 
     private void parseConfig(){
 
@@ -23,7 +45,6 @@ public class Randomizer {
             InputStream in = new FileInputStream("src/com/company/res/config.txt");
             prop.load(in);
             in.close();
-            //System.out.println(prop.getProperty("amount"));
             this.outputAmount = Integer.parseInt(prop.getProperty("amount"));
         }
         catch(Exception e) {
@@ -31,6 +52,11 @@ public class Randomizer {
             System.exit(1);
         }
     }
+
+    /**
+     * Randomizes a set of animes, adds them to a new list and removes them from the old to avoid duplicate results.
+     * @return A list of random anime.
+     */
 
     public ArrayList<Anime> randomizeAnime(){
         int listMax = animeList.size();
@@ -43,11 +69,7 @@ public class Randomizer {
             returnList.add(animeList.get(rolledNumber));
             animeList.remove(rolledNumber);
             listMax--;
-
-
         }
         return returnList;
     }
-
-
 }
