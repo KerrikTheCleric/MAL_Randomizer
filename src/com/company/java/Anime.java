62a7ebfcc0a76da3f5105d9632247d3a37a34a6b
Victor Gustafsson <dv16vgn@cs.umu.se>
2020-01-13 20:55:40 +0100

    Commented Anime class.

--- src/com/company/java/Anime.java
+++ src/com/company/java/Anime.java
@@ -1,7 +1,31 @@
 public class Anime {
+
+    /**
+     * <h1>Anime</h1>
+     * A data representation of weeb cartoons.
+     * <p>
+     * name - The name of the anime.
+     * <p>
+     * episodes - The amount of episodes.
+     * <p>
+     * status - The status of the anime.
+     * <p>
+     *
+     * @author  Victor Gustafsson
+     * @version 1.0
+     * @since   2020-01-13
+     */
+
     String name;
     int episodes;
     String status;
+
+    /**
+     * Standard constructor.
+     * @param name The name of the anime.
+     * @param episodes The amount of episodes.
+     * @param status The status of the anime.
+     */
 
     public Anime(String name, int episodes, String status) {
         this.name = name;
